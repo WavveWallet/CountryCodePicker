700ee53d466ccbcacf4a82f4dddb439b26a5f80f
Segun Sodimu <princesegzy01@yahoo.com>
2021-08-04 02:28:57 +0100

    re-arrange countries

--- lib/country_codes.dart
+++ lib/country_codes.dart
@@ -1,13 +1,11 @@
 const List<Map<String, String>> codes = [
-
-  {
-    "name": "Ghana",
-    "code": "GH",
-    "dial_code": "+233",
-  },
   {
     "name": "Nigeria",
     "code": "NG",
     "dial_code": "+234",
+  },{
+    "name": "Ghana",
+    "code": "GH",
+    "dial_code": "+233",
   },
 ];
